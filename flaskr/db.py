1e827bbd1caf0f4d5ae86ea7e179bc7076c241da
Gavin Dale <gavin.dale@nextiva.com>
2020-02-07 16:30:26 -0700

    adding auth blueprint

--- flaskr/db.py
+++ flaskr/db.py
@@ -7,7 +7,7 @@ def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
                 current_app.config['DATABASE'],
-                detect_types=sqlite3.PARSE_DECLTYLES
+                detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
 
@@ -22,7 +22,7 @@ def close_db(e=None):
 def init_db():
     db = get_db()
 
-    with current_app.open_resources('schema.sql') as f:
+    with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
 @click.command('init-db')
@@ -31,3 +31,9 @@ def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
